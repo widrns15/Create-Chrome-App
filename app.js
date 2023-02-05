76c7ce7abaab67f761ab04e04123323cf37d517b
widrns15 <widrns15@naver.com>
2023-02-05 23:02:33 +0900

    🔥 feat: 홈페이지 글자 클릭 시, 파란색으로 변경 기능 추가

---
+++ app.js
@@ -0,0 +1,7 @@
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);
