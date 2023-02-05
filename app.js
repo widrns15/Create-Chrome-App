8788d64fbadc2da42463c60d6d911499ea3b5794
widrns15 <widrns15@naver.com>
2023-02-05 23:36:38 +0900

    🔥 feat: window 관련 기능 추가
    1. 윈도우 창 크기 조절 시, 배경 색 변경
    2. 복사 시, 경고창 출력
    3. 와이파이 껐을 때, 경고창 출력
    3. 와이파이 켰을 때, 경고창 출력

--- app.js
+++ app.js
@@ -4,4 +4,27 @@ function handleTitleClick() {
   title.style.color = "blue";
 }
 
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier");
+}
+
+function handleWindowOffline() {
+  alert("SOS No WIFI");
+  console.log("NO WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOD");
+  console.log("GOOD WIFI");
+}
+
 title.addEventListener("click", handleTitleClick);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
