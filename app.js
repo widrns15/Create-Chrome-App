79e5bdacc2b3bfdd55ba638f5c336f9d84762eaf
widrns15 <widrns15@naver.com>
2023-02-06 19:59:54 +0900

    🥽 test: 홈페이지 글자 클릭 시, 파란색과 토마토색 반복 변경 기능 추가

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier");
-}
-
-function handleWindowOffline() {
-  alert("SOS No WIFI");
-  console.log("NO WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOD");
-  console.log("GOOD WIFI");
-}
-
-title.addEventListener("click", handleTitleClick);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
