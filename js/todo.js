23b2e2ff6bf6e6c087c77fe4f22b2e988a5d248a
widrns15 <widrns15@naver.com>
2023-02-26 20:51:21 +0900

    🔥 feat: 홈페이지에 To Do List 기능 추가

---
+++ js/todo.js
@@ -0,0 +1,20 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newToDo;
+  li.appendChild(span);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newToDo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
