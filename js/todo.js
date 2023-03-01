d354e52703805b2ba7359b877d87f64b7ee5ca94
widrns15 <widrns15@naver.com>
2023-03-01 18:51:10 +0900

    🛠️ fix: 홈페이지 새로고침 시, 기존 작성했던 데이터가 복원되도록 수정
    - 데이터 값을 localStorage에 저장하여 새로고침 시, 데이터 복원되도록 수정

--- js/todo.js
+++ js/todo.js
@@ -7,6 +7,14 @@ function deleteToDo(event) {
   li.remove();
 }
 
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
@@ -23,7 +31,17 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newToDo);
   paintToDo(newToDo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
