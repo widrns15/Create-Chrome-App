a3cdacf169e952bbaf83d9382f8ad0b8ef30bd1c
widrns15 <widrns15@naver.com>
2023-03-05 00:14:17 +0900

    🎨 design: 전체적으로 홈페이지 디자인
    - Html, JS 코드 리팩토링

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const toDo = document.querySelector("#todo-form");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -14,8 +15,9 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello~! ${username}`;
+  greeting.innerText = `Welcome~! ${username} 💕`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  toDo.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
